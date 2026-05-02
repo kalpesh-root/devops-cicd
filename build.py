a9812a58a7809841939a6596c0e886e000a3af0c
kalpesh-root <kalpeshdevops@gmail.com>
2026-05-02 20:34:23 +0530

    py test 1

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import os
 import json
 import subprocess
 
-SERVICEFILE = "/configs/service.json"
+SERVICEFILE = "/home/kp/devops-cicd/configs/service.json"
 
 def ReadConfigs ():
     with open(SERVICEFILE, 'r') as file:
