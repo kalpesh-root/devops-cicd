379350973379223c0492c4d9d079d3a50dcc945a
kalpesh-root <kalpeshdevops@gmail.com>
2026-05-16 11:18:30 +0530

    16-May-first commit

--- build.py
+++ build.py
@@ -1,43 +1,36 @@
-#! /bin/python3
 import os
 import json
-import subprocess
+
 
 SERVICEFILE = "devops/configs/service.json"
+
+
+def Getenv ():
+    return {
+    "SERVICE":os.getenv("SERVICE"),
+    "BRANCH_NAME":os.getenv("BRANCH_NAME"),
+    "RELEASE_NAME":os.getenv("RELEASE_NAME"),
+    "SERVICE":os.getenv("SERVICE")
+    }
 
 def ReadConfigs ():
     with open(SERVICEFILE, 'r') as file:
         jsondata = json.load(file)
     return jsondata
 
-def GetEnvs ():
-    return {
-    "BRANCH_NAME":os.getenv("BRANCH_NAME"),
-    "RELEASE_NAME":os.getenv("RELEASE_NAME"),
-    "SERVICE":os.getenv("SERVICE"),
-    "DEVOPS_DIR":os.getenv("DEVOPS_DIR")
-    }
-
-def BuildService(env, config):
-    SERVICE = env["SERVICE"]
+def Builddtl (env,config):
+    SERVICE = env("SERVICE")
     BUILD_CMD = config[SERVICE]["build_cmd"]
     BUILD_TYPE = config[SERVICE]["build_type"]
     return BUILD_CMD, BUILD_TYPE
+    
 
-
-
-def BuildApps():
-    config = ReadConfigs()
-    env = GetEnvs()
+def BuildService ():
+    env = Getenv ()
+    config = ReadConfigs ()
     service_path = os.path.join(os.getcwd(), "app", env["SERVICE"])
-    BUILD_CMD, BUILD_TYPE  = BuildService(env,config )
+    BUILD_CMD, BUILD_TYPE  = Builddtl(env,config )
     print ("Executing this command:",BUILD_CMD )
 
-    os.chdir(service_path)
-
-    subprocess.run(BUILD_CMD, shell=True, check=True)
-
 if __name__ == "__main__":
-    BuildApps()
-
-    
+    BuildService()
